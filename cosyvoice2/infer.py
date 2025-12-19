09e2d183cbc8409212beee1fde5823dc0729a386
zhangdejun <18263950064@163.com>
2025-12-19 17:52:12 +0800

    update

--- cosyvoice2/infer.py
+++ cosyvoice2/infer.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../../../third_party/Matcha-TTS')
+sys.path.append('./third_party/Matcha-TTS')
 from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
 from cosyvoice.utils.file_utils import load_wav
 import torchaudio
